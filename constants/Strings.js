05adb4bcd23552031e21d4ada1e2ca59605a4615
Theresa Benkman <konatess@gmail.com>
2023-04-25 01:43:47 -0700

    Added strings for data limit errors.

--- constants/Strings.js
+++ constants/Strings.js
@@ -96,12 +96,12 @@ export default {
             ingTableHave: "Have:"
         },
         messages: {
-            ingredients: "Choose an ingredient: ",
             ingNameTooShort: "Your ingredient needs a name",
             ingNameBadChar: "Ingredient names can only contain letters, numbers, spaces, and these symbols: '-_/&:!()",
             ingUnitTooShort: "Your ingredient needs a unit of measurement. How do you count it? By item, pair, skein, box, bottle...",
             ingUnitBadChar: "Unit names can't contain special characters",
             ingCostZero: "Your ingredient should have a cost. Even if you didn't pay for it, look up what it would cost to buy.",
+            
             ingPerItem: "How much of this ingredient do you use to make one of this product? You're counting by *unit*.",
             prodNameShort: "Your product needs a name",
             prodNameBadChar: "Product names can only contain letters, numbers, spaces, and these symbols: '-_/&:!()",
@@ -109,6 +109,8 @@ export default {
             prodTime: "Please set a time of at least one minute and less than 100 hours",
             prodAmount: "Please set an amount greater than 0 and less than 1,000,000",
             ingredientsAmounts: "Please set amounts for ingredients, or remove them from the list",
+            
+            ingredients: "Choose an ingredient: ",
             deleteAllIng: "Are you sure you want to delete all your ingredients? This cannot be undone.",
             deleteAllRec: "Are you sure you want to delete all your recipes? This cannot be undone.",
             overwriteLocal: "Are you sure you want to overwrite your local data with your database data? This cannot be undone.",
@@ -119,7 +121,11 @@ export default {
             wage: "You deserve to be paid a living wage. This number is what you would want to be paid per hour. Most humans are their own worst critic, so if that applies to you, be sure to go a little higher than you are comfortable with.",  //If you make this product yourself, the wage is what you should pay yourself for time spent making this specific product. If you pay an employee to make this product, put their wage here.
             profit: "Your business needs profit that is separate from the hourly wage above. This number should be high enough to cover:\n• expected expenses tied to each product such as fees from credit card purchases or sales platforms\n• expected general expenses such as subscription fees which are spread over all your sales\n• and still have some left over for unexpected expenses.\nIf all of that feels too overwhelming or unpredictable, start by putting 50 in the % field, and refine from there. You can always update your profits later.",
             inventory: "This is the number of this product you have in inventory now, created but not sold. This number can be 0. Use the 'Calculate Ingredients Needed' button below to see how much of your ingredients you would need to make # more of this product, and how much of each ingredient you already have.",
-            ingInfo: "Use the 'Add Ingredients' button to choose ingredients needed for this recipe, then touch each ingredient to update the quantity."
+            ingInfo: "Use the 'Add Ingredients' button to choose ingredients needed for this recipe, then touch each ingredient to update the quantity.",
+            dataLimit: {
+                ing: "You've reached your limit for creating and saving ingredients. Would you like to upgrade your subscription to create more?",
+                rec: "You've reached your limit for creating and saving recipes. Would you like to upgrade your subscription to create more?",
+            },
         },
         placeholder:  {
             email: "email@example.com",
