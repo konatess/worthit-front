2c4b3a299588ffc98725a49df665121f0951e771
Theresa Benkman <konatess@gmail.com>
2023-04-27 23:00:36 -0700

    Reorganized app.config.js for debug reasons.

--- app.config.js
+++ app.config.js
@@ -1,18 +1,18 @@
 export default {
     expo: {
+        android: {
+            googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+            package: "com.buddingapps.worthit",
+        },
+        ios: {
+            googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
+            bundleIdentifier: "com.buddingapps.worthit"
+        },
         extra: {
           eas: {
             projectId: "9f68bef9-2d97-4362-b1a3-3e0d3e16f7cc"
           }
         }
     },
-    android: {
-        googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
-        package: "com.buddingapps.worthit",
-    },
-    ios: {
-        googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
-        bundleIdentifier: "com.buddingapps.worthit"
-    }
   };
   
