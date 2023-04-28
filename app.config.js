bafbb0c51002d30a50fa7eb40112f3f07eae1f88
Theresa Benkman <konatess@gmail.com>
2023-04-27 22:58:55 -0700

    Added android.package and ios.bundleIdentifier to app.config.js.

--- app.config.js
+++ app.config.js
@@ -8,9 +8,11 @@ export default {
     },
     android: {
         googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+        package: "com.buddingapps.worthit",
     },
     ios: {
         googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
+        bundleIdentifier: "com.buddingapps.worthit"
     }
   };
   
