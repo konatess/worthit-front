5ac6f33a32144d7f8ac8816625a459ce7efc7911
Theresa Benkman <konatess@gmail.com>
2023-04-27 22:51:23 -0700

    Moved googleServieFile references to app.config.js.

---
+++ app.config.js
@@ -0,0 +1,9 @@
+export default {
+    android: {
+        googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    },
+    ios: {
+        googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
+    }
+  };
+  
