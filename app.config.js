2c88d7601d57e2f5804abcc72ea520af2836b2ca
Theresa Benkman <konatess@gmail.com>
2023-04-27 22:55:39 -0700

    Updated projectId in app.config.js.

--- app.config.js
+++ app.config.js
@@ -1,4 +1,11 @@
 export default {
+    expo: {
+        extra: {
+          eas: {
+            projectId: "9f68bef9-2d97-4362-b1a3-3e0d3e16f7cc"
+          }
+        }
+    },
     android: {
         googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
     },
