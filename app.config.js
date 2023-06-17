01f654aff860843a5f614605600b311aeb48db04
Theresa Benkman <konatess@gmail.com>
2023-06-17 00:38:28 -0700

    updated version numbers.

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
         name: "Worth It",
         slug: "worthit",
         scheme: "worthit",
-        version: "1.0.24",
+        version: "1.0.25",
         orientation: "portrait",
         icon: "./assets/icon.jpg",
         userInterfaceStyle: "light",
@@ -34,7 +34,7 @@ export default {
                     }
                 ]
             },
-            buildNumber: "25",
+            buildNumber: "26",
             bundleIdentifier: "com.buddingapps.worthit",
             googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
         },
@@ -43,7 +43,7 @@ export default {
                 foregroundImage: "./assets/adaptive-icon.png",
                 backgroundColor: "#FFFAEF"
             },
-            versionCode: 25,
+            versionCode: 26,
             permissions: [
                 "android.permission.INTERNET"
             ],
