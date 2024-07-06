f4a54a102d9b8d1f5c6b2873eb0b8ce3d9bcbf71
Theresa Benkman <konatess@gmail.com>
2024-07-06 04:14:59 -0700

    Added default values to settings because language ref was failing in several places.

--- constants/SettingsContext.js
+++ constants/SettingsContext.js
@@ -1,3 +1,10 @@
 import { createContext } from "react";
+import Strings from "./Strings";
 
-export const SettingsContext = createContext({});
+export const SettingsContext = createContext({
+    darkMode: false, 
+    currency: Strings.util.currencies[0], 
+    language: Strings.util.languages[0], 
+    login: Strings.util.logins[0],
+    decimalLength: 2
+});
